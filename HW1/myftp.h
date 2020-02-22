737c362c7670d7e00e4494ce9bf1232945039c50
Nulllun <michaeld23046e123@gmail.com>
2020-02-22 18:52:19 +0800

    create 5 files

---
+++ HW1/myftp.h
@@ -0,0 +1,19 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define PORT 12345
+#define IPADDR "127.0.0.1"
+
+struct message_s {
+    unsigned char protocol[5];
+    unsigned char type;
+    unsigned int length;
+} __attribute__ ((packed));
