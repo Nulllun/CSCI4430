47419dc5c79c6418bb6f7932eb58312c64bc64a2
Adam <adamandworking@gmail.com>
2020-04-01 18:34:37 +0800

    done reading config

--- HW2/myftpclient.c
+++ HW2/myftpclient.c
@@ -1,4 +1,4 @@
-# include "myftp.c"
+#include "myftp.c"
 
 int sd;
 char *server_ip_addr;
@@ -6,7 +6,8 @@ char *port;
 char *mode;
 char *filename;
 
-void list_request() {
+void list_request()
+{
     send_header(sd, 0xa1, 0);
     struct message_s *header = recv_header(sd);
     int payload_len = header->length - HEADER_LEN;
@@ -16,17 +17,20 @@ void list_request() {
     free(buff);
 }
 
-void get_request() {
+void get_request()
+{
     send_header(sd, 0xb1, strlen(filename) + 1);
     send_payload(sd, (void *)filename, strlen(filename) + 1);
     struct message_s *header = recv_header(sd);
     unsigned char type = header->type;
-    if(type == 0xb3) {
+    if (type == 0xb3)
+    {
         // the file does not exist
         printf("The requested file does not exist.\n");
         return;
     }
-    if(type == 0xb2) {
+    if (type == 0xb2)
+    {
         printf("The download process will begin soon.\n");
     }
 
@@ -36,10 +40,12 @@ void get_request() {
 
     int file_size = header->length - HEADER_LEN;
     void *buff = recv_payload(sd, file_size);
-    if(fwrite(buff, file_size, 1, fptr) == 0) {
+    if (fwrite(buff, file_size, 1, fptr) == 0)
+    {
         printf("Download fail.\n");
     }
-    else {
+    else
+    {
         printf("Download success.\n");
     }
     free(header);
@@ -47,10 +53,12 @@ void get_request() {
     fclose(fptr);
 }
 
-void put_request() {
+void put_request()
+{
     int file_size;
-    struct stat st;  
-    if (stat(filename, &st) == 0) {
+    struct stat st;
+    if (stat(filename, &st) == 0)
+    {
         // the file exist
         file_size = st.st_size;
         send_header(sd, 0xc1, strlen(filename) + 1);
@@ -69,16 +77,56 @@ void put_request() {
         free(buff);
         fclose(fptr);
     }
-    else {
+    else
+    {
         printf("%s does not exist in local.\n", filename);
     }
-
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     // setup connection
-    char *ip_addr = argv[1];
+    char *ip_addr = argv[1];  //Adam: these variables ip_addr and port need to connect the variables list_of_IP and list_of_port below
     int port = atoi(argv[2]);
+
+    printf("reading %s\n", argv[1]);
+    FILE *fp;
+    char buff[255];
+    fp = fopen(argv[1], "r");
+    fgets(buff, 255, (FILE *)fp);
+    int n = atoi(buff);
+    fgets(buff, 255, (FILE *)fp);
+    int k = atoi(buff);
+    fgets(buff, 255, (FILE *)fp);
+    int block_size = atoi(buff);
+
+    char delim[] = ":";
+    char list_of_IP[255][255];
+    int list_of_port[255];
+    int count = 0;
+    while (fgets(buff, sizeof(buff), (FILE *)fp) != 0)
+    {
+        char *ptr = strtok(buff, delim);
+        memcpy(list_of_IP[count], ptr, strlen(ptr) + 1);
+        ptr = strtok(NULL, delim);
+        list_of_port[count] = atoi(ptr);
+        count++;
+    }
+    printf("This is the read var:\n");
+    printf("n : %d\n", n);
+    printf("k : %d\n", k);
+    printf("block_size : %d\n", block_size);
+    int print_count = 0;
+    for (print_count = 0; print_count < count; print_count++)
+    {
+        printf("list %d\n", print_count);
+        printf("IP : %s\n", list_of_IP[print_count]);
+        printf("port : %d\n\n", list_of_port[print_count]);
+    }
+    fclose(fp);
+
+    printf("finish reading var\n");
+
     sd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server_addr;
     pthread_t worker;
@@ -86,28 +134,33 @@ int main(int argc, char **argv) {
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr(ip_addr);
     server_addr.sin_port = htons(port);
-    if (connect(sd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (connect(sd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
         printf("connection error: %s (Errno:%d)\n", strerror(errno), errno);
         exit(0);
     }
 
     // three mode
     mode = argv[3];
-    if(argc >= 5) {
+    if (argc >= 5)
+    {
         filename = argv[4];
     }
     // printf("Mode: %s\n", mode);
     // printf("Filename: %s\n", filename);
-    if(strcmp(mode, "list") == 0){
+    if (strcmp(mode, "list") == 0)
+    {
         list_request();
     }
-    else if(strcmp(mode, "put") == 0){
+    else if (strcmp(mode, "put") == 0)
+    {
         put_request();
     }
-    else if(strcmp(mode, "get") == 0){
+    else if (strcmp(mode, "get") == 0)
+    {
         get_request();
     }
-    
+
     close(sd);
     return 0;
 }
--- HW2/myftpserver.c
+++ HW2/myftpserver.c
@@ -1,6 +1,7 @@
-# include "myftp.c"
+#include "myftp.c"
 
-void list_reply(int sd) {
+void list_reply(int sd)
+{
     char list_dir[500];
     int payload_len;
     listdir(list_dir);
@@ -9,15 +10,17 @@ void list_reply(int sd) {
     send_payload(sd, list_dir, payload_len);
 }
 
-void get_reply(int sd, int payload_len) {
+void get_reply(int sd, int payload_len)
+{
     int file_size;
-    struct stat st;  
+    struct stat st;
     char *filename = (char *)recv_payload(sd, payload_len);
     char final_path[6 + strlen(filename)];
     strcpy(final_path, "data/");
     strcat(final_path, filename);
     printf("Requested filename: %s\n", filename);
-    if (stat(final_path, &st) == 0) {
+    if (stat(final_path, &st) == 0)
+    {
         // the file exist
         file_size = st.st_size;
         send_header(sd, 0xb2, 0);
@@ -31,14 +34,16 @@ void get_reply(int sd, int payload_len) {
         free(buff);
         fclose(fptr);
     }
-    else {
+    else
+    {
         send_header(sd, 0xb3, 0);
     }
 
     free(filename);
 }
 
-void put_reply(int sd, int payload_len) {
+void put_reply(int sd, int payload_len)
+{
     char *filename = (char *)recv_payload(sd, payload_len);
     char final_path[6 + strlen(filename)];
     strcpy(final_path, "data/");
@@ -48,10 +53,12 @@ void put_reply(int sd, int payload_len) {
     int file_size = header->length - HEADER_LEN;
     void *buff = recv_payload(sd, file_size);
     FILE *fptr = fopen(final_path, "wb");
-    if(fwrite(buff, file_size, 1, fptr) == 0) {
+    if (fwrite(buff, file_size, 1, fptr) == 0)
+    {
         printf("Download fail.\n");
     }
-    else {
+    else
+    {
         printf("Download success.\n");
     }
     free(header);
@@ -59,22 +66,26 @@ void put_reply(int sd, int payload_len) {
     fclose(fptr);
 }
 
-void *pthread_prog(void *sDescriptor) {
+void *pthread_prog(void *sDescriptor)
+{
     int sd = *(int *)sDescriptor;
     struct message_s *header = recv_header(sd);
     // printf("Received Header:\n");
     // printf("protocol: %s", header->protocol);
     // printf("type: 0x%x\n", header->type);
     // printf("length: %u\n", header->length);
-    if(header->type == 0xa1){
+    if (header->type == 0xa1)
+    {
         printf("List request received.\n");
         list_reply(sd);
     }
-    else if(header->type == 0xb1){
+    else if (header->type == 0xb1)
+    {
         printf("Get request received.\n");
         get_reply(sd, header->length - HEADER_LEN);
     }
-    else if(header->type == 0xc1){
+    else if (header->type == 0xc1)
+    {
         printf("Put request received.\n");
         put_reply(sd, header->length - HEADER_LEN);
     }
@@ -82,18 +93,35 @@ void *pthread_prog(void *sDescriptor) {
     pthread_exit(NULL);
 }
 
-int main(int argc, char **argv) {
-    int port = atoi(argv[1]);
-    printf("%s\n", argv[1]);
+int main(int argc, char **argv)
+{
+    printf("reading %s\n", argv[1]);
     FILE *fp;
-    // fp = fopen("/tmp/test.txt", "w+");
-    // fprintf(fp, "This is testing for fprintf...\n");
-    // fputs("This is testing for fputs...\n", fp);
-    // fclose(fp);
+    char buff[255];
+    fp = fopen(argv[1], "r");
+    fgets(buff, 255, (FILE *)fp);
+    int n = atoi(buff);
+    fgets(buff, 255, (FILE *)fp);
+    int k = atoi(buff);
+    fgets(buff, 255, (FILE *)fp);
+    int the_ID = atoi(buff);
+    fgets(buff, 255, (FILE *)fp);
+    int block_size = atoi(buff);
+    fgets(buff, 255, (FILE *)fp);
+    int port = atoi(buff);
+    printf("This is the read var:\n");
+    printf("n : %d\n", n);
+    printf("k : %d\n", k);
+    printf("the_ID : %d\n", the_ID);
+    printf("block_size : %d\n", block_size);
+    printf("port : %d\n", port);
+    fclose(fp);
+    printf("finish reading var\n");
     int config_location = atoi(argv[2]);
     int sd = socket(AF_INET, SOCK_STREAM, 0);
     long val = 1;
-    if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(long)) == -1) {
+    if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(long)) == -1)
+    {
         perror("setsockopt");
         exit(1);
     }
@@ -102,23 +130,29 @@ int main(int argc, char **argv) {
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(port);
-    if (bind(sd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (bind(sd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
         printf("bind error: %s (Errno:%d)\n", strerror(errno), errno);
         exit(0);
     }
-    if (listen(sd, 10) < 0) {
+    if (listen(sd, 10) < 0)
+    {
         printf("listen error: %s (Errno:%d)\n", strerror(errno), errno);
         exit(0);
     }
 
-    while(1) {
+    while (1)
+    {
         int client_sd;
         struct sockaddr_in client_addr;
         int addr_len = sizeof(client_addr);
-        if ((client_sd = accept(sd, (struct sockaddr *)&client_addr, (socklen_t *)&addr_len)) <0) {
+        if ((client_sd = accept(sd, (struct sockaddr *)&client_addr, (socklen_t *)&addr_len)) < 0)
+        {
             printf("accept erro: %s (Errno:%d)\n", strerror(errno), errno);
             // exit(0);
-        } else {
+        }
+        else
+        {
             printf("receive connection from %s\n", inet_ntoa(client_addr.sin_addr));
         }
         pthread_t worker;
